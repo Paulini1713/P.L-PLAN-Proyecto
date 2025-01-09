46b237843492305f318ec0811301fae257b0f0c5
Paulini1713 <paulafrankfurt@gmail.com>
2025-01-09 19:09:38 +0100

    uso camelCAse y handler

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,100 +1,232 @@
 'use strict';
 
-(function () {
+//(function () {
     // Añado botón svg en pantallas pequeñas con un menú
-    let headerButton = document.querySelector('.header-button');
-    let ulNav = document.querySelector('.ul-nav');
+    //let headerButton = document.querySelector('.header-button');
+    //let ulNav = document.querySelector('.ul-nav');
+    //console.log(headerButton);
+    //console.log(ulNav);
+
+    //headerButton.addEventListener('click', function () {
+        //ulNav.classList.toggle('isVisible');
+    //});
+
+    // Función para el carrusel de Paula
+    //let Paula = 0;
+
+    //function mostrarSlidePaula(n) {
+       // const slidesPaula = document.querySelectorAll('.div-ella .div-item');
+        //if (n >= slidesPaula.length) {
+           // paula = 0;
+        //} else if (n < 0) {
+            //paula = slidesPaula.length - 1;
+        //} else {
+            //paula = n;
+        //}
+
+        // Resaltar imagen activa
+        //slidesPaula.forEach((slide, i) => {
+            //slide.classList.remove('active');
+            //if (i === paula) {
+                //slide.classList.add('active');
+           // }
+        //});
+    //}
+
+    //function cambiarSlidePaula(n) {
+        //mostrarSlidePaula(paula + n);
+    //}
+
+    // Iniciar el primer slide de Paula
+    //mostrarSlidePaula(paula);
+
+    // Botones navegación Paula
+    //document.querySelector('.section-nosotros .prev').addEventListener('click', () => cambiarSlidePaula(-1));
+    //document.querySelector('.section-nosotros .next').addEventListener('click', () => cambiarSlidePaula(1));
+
+    // Carrusel de Mi Historia
+    //let historia = 0;
+
+    //function mostrarSlideHistoria(n) {
+        //const slidesHistoria = document.querySelectorAll('.div-juntos .div-item');
+        //if (n >= slidesHistoria.length) {
+            //historia = 0;
+        //} else if (n < 0) {
+           // historia = slidesHistoria.length - 1;
+        //} else {
+            //historia = n;
+        //}
+
+        // Resaltar la imagen activa
+        //slidesHistoria.forEach((slide, i) => {
+            //slide.classList.remove('active');
+            //if (i === historia) {
+                //slide.classList.add('active');
+            //}
+        //});
+    //}
+
+    //function cambiarSlideHistoria(n) {
+        //mostrarSlideHistoria(historia + n);
+    //}
+
+    // Iniciar primer slide de Historia
+    //mostrarSlideHistoria(historia);
+
+    // Botones navegación Historia
+    //document.querySelector('.section-juntos .prev').addEventListener('click', () => cambiarSlideHistoria(-1));
+    //document.querySelector('.section-juntos .next').addEventListener('click', () => cambiarSlideHistoria(1));
+
+    // Botones de navegación
+    //document.querySelector('.prev').addEventListener('click', () => cambiarSlide(-1));
+    //document.querySelector('.next').addEventListener('click', () => cambiarSlide(1));
+
+    // Manejo de botones y párrafos
+    //const boton = document.querySelectorAll('.li-botones');
+    //const parrafo = document.querySelectorAll('.parrafo-p');
+    //console.log(boton, parrafo);
+
+   // boton.forEach((_, i) => {
+        //boton[i].addEventListener('click', () => {
+            //boton.forEach((_, i) => {
+                //boton[i].classList.remove('isVisible');
+                //parrafo[i].classList.remove('isVisible');
+            //});
+
+            //boton[i].classList.add('isVisible');
+            //parrafo[i].classList.add('isVisible');
+        //});
+    //});
+//})();
+
+
+
+(function () {
+
+    // Función para manejar los eventos
+    function handler( event ) {
+
+        switch (event.target.className) {
+            case 'header-button':
+                ulNav.classList.toggle( 'isVisible' );
+                break;
+
+            case 'prev':
+                if (event.target.closest('.section-nosotros')) {
+                    cambiarSlidePaula(-1);
+                } else if (event.target.closest('.section-juntos')) {
+                    cambiarSlideHistoria(-1);
+                } else {
+                    cambiarSlide(-1);
+                }
+                break;
+
+            case 'next':
+                if (event.target.closest('.section-nosotros')) {
+                    cambiarSlidePaula(1);
+                } else if (event.target.closest('.section-juntos')) {
+                    cambiarSlideHistoria(1);
+                } else {
+                    cambiarSlide(1);
+                }
+                break;
+
+            case 'li-botones':
+                const index = Array.from( boton ).indexOf(event.target);
+                boton.forEach((btn, i) => {
+                    btn.classList.remove('isVisible');
+                    parrafo[i].classList.remove('isVisible');
+                });
+                boton[index].classList.add('isVisible');
+                parrafo[index].classList.add('isVisible');
+                break;
+        }
+    }
+
+    // Añado botón svg en pantallas pequeñas con un menú
+    const headerButton = document.querySelector('.header-button');
+    const ulNav = document.querySelector('.ul-nav');
     console.log(headerButton);
     console.log(ulNav);
 
-    headerButton.addEventListener('click', function () {
-        ulNav.classList.toggle('isVisible');
-    });
+    headerButton.addEventListener('click', handler);
 
     // Función para el carrusel de Paula
-    let Paula = 0;
+    let paula = 0;
+    const slidesPaula = document.querySelectorAll('.div-ella .div-item');
 
     function mostrarSlidePaula(n) {
-        const slidesPaula = document.querySelectorAll('.div-ella .div-item');
         if (n >= slidesPaula.length) {
-            Paula = 0;
+            paula = 0;
         } else if (n < 0) {
-            Paula = slidesPaula.length - 1;
+            paula = slidesPaula.length - 1;
         } else {
-            Paula = n;
+            paula = n;
         }
 
         // Resaltar imagen activa
         slidesPaula.forEach((slide, i) => {
             slide.classList.remove('active');
-            if (i === Paula) {
+            if (i === paula) {
                 slide.classList.add('active');
             }
         });
     }
 
     function cambiarSlidePaula(n) {
-        mostrarSlidePaula(Paula + n);
+        mostrarSlidePaula(paula + n);
     }
 
     // Iniciar el primer slide de Paula
-    mostrarSlidePaula(Paula);
+    mostrarSlidePaula(paula);
 
     // Botones navegación Paula
-    document.querySelector('.section-nosotros .prev').addEventListener('click', () => cambiarSlidePaula(-1));
-    document.querySelector('.section-nosotros .next').addEventListener('click', () => cambiarSlidePaula(1));
+    document.querySelector('.section-nosotros .prev').addEventListener('click', handler);
+    document.querySelector('.section-nosotros .next').addEventListener('click', handler);
 
     // Carrusel de Mi Historia
-    let Historia = 0;
+    let historia = 0;
+    const slidesHistoria = document.querySelectorAll('.div-juntos .div-item');
 
     function mostrarSlideHistoria(n) {
-        const slidesHistoria = document.querySelectorAll('.div-juntos .div-item');
         if (n >= slidesHistoria.length) {
-            Historia = 0;
+            historia = 0;
         } else if (n < 0) {
-            Historia = slidesHistoria.length - 1;
+            historia = slidesHistoria.length - 1;
         } else {
-            Historia = n;
+            historia = n;
         }
 
-        // Resaltar la imagen activa
+        // Resaltar imagen activa
         slidesHistoria.forEach((slide, i) => {
             slide.classList.remove('active');
-            if (i === Historia) {
+            if (i === historia) {
                 slide.classList.add('active');
             }
         });
     }
 
     function cambiarSlideHistoria(n) {
-        mostrarSlideHistoria(Historia + n);
+        mostrarSlideHistoria(historia + n);
     }
 
     // Iniciar primer slide de Historia
-    mostrarSlideHistoria(Historia);
+    mostrarSlideHistoria(historia);
 
     // Botones navegación Historia
-    document.querySelector('.section-juntos .prev').addEventListener('click', () => cambiarSlideHistoria(-1));
-    document.querySelector('.section-juntos .next').addEventListener('click', () => cambiarSlideHistoria(1));
+    document.querySelector('.section-juntos .prev').addEventListener('click', handler);
+    document.querySelector('.section-juntos .next').addEventListener('click', handler);
 
     // Botones de navegación
-    document.querySelector('.prev').addEventListener('click', () => cambiarSlide(-1));
-    document.querySelector('.next').addEventListener('click', () => cambiarSlide(1));
+    document.querySelector('.prev').addEventListener('click', handler);
+    document.querySelector('.next').addEventListener('click', handler);
 
     // Manejo de botones y párrafos
     const boton = document.querySelectorAll('.li-botones');
     const parrafo = document.querySelectorAll('.parrafo-p');
     console.log(boton, parrafo);
 
-    boton.forEach((_, i) => {
-        boton[i].addEventListener('click', () => {
-            boton.forEach((_, i) => {
-                boton[i].classList.remove('isVisible');
-                parrafo[i].classList.remove('isVisible');
-            });
-
-            boton[i].classList.add('isVisible');
-            parrafo[i].classList.add('isVisible');
-        });
+    boton.forEach((btn) => {
+        btn.addEventListener('click', handler);
     });
 })();
